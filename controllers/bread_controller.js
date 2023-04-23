b5fc891d518209ffb4d3ec5c7e4a4dbe5dd385a4
Brenda <amaya.brendaa@gmail.com>
2023-04-23 12:08:41 -0700

    Added bread data model

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const breads = express.Router();
+const breaD = require('../models/bread.js')
 
-//INDEX
+//INDEX - READ ALL
 
 breads.get('/', (req,res) =>{
-    res.send('This is the index at /breads');
+    res.send(breaD);
 })
+
+//SHOW - READ ONE?
+breads.get('/:arraryIndex', (req,res) => {
+    const arraryIndex = req.params.arraryIndex;
+    res.send(breaD[arraryIndex]);
+})
+
 
 //EXPORT
 module.exports = breads;
